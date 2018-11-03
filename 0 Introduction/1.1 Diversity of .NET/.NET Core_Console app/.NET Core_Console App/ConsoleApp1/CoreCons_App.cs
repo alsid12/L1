275c375e29f3f26f5981f65db0a796f1c14d20e3
Aliaksandr_Sidarau <aliaksandr_sidarau@epam.com>
2018-11-03 14:34:15 +0300

    Added solution for module "Introduction to .NET"

---
+++ 0 Introduction/1.1 Diversity of .NET/.NET Core_Console app/.NET Core_Console App/ConsoleApp1/CoreCons_App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Console_App
+    {
+        private static void Main(string[] args)
+        {
+            //if (args.Length == 0)
+            //    {
+            //        Console.WriteLine("Please enter your name.");
+            //        args[0] = Console.ReadLine();
+            //    }
+            Console.WriteLine("Hello, {0}!", args[0]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 0 Introduction/1.1 Diversity of .NET/.NET Frmwrk_Console/Frmwrk_Console/Frmwrk_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frmwrk_Console
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //args[0] = Console.ReadLine();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter your name.");
+                args[0] = Console.ReadLine().ToString();
+            }
+            Console.WriteLine("Hello, {0}!", args[0]);
+            Console.ReadKey();
+        }
+    }
+}
